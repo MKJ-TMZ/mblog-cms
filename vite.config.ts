a1c01fad39f0652b888d9c0ee0e28571e6674d0a
MTCode <1318844659@qq.com>
2022-02-28 22:55:19 +0800

    使用ElementPlus按需导入

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from "path";
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
   server: {
@@ -13,5 +16,13 @@ export default defineConfig({
     },
     extensions: ['.js', '.json', '.ts']
   },
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ]
 })
