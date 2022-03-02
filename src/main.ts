3bb09dcbb0c71317e638b4d127b6d03ce5bb7746
MTCode <1318844659@qq.com>
2022-03-02 23:58:08 +0800

    引入阿里矢量图标

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import router from './router'
 import store from "@/store";
 import 'element-plus/dist/index.css'
 import './assets/less/base.less'
+import './assets/lib/icon/iconfont.css'
 
 const app = createApp(App)
 
