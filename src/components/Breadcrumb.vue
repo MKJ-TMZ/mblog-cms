4c0efdbc557e334bdb9f3ed449b654c17e870a06
MTCode <1318844659@qq.com>
2022-03-03 23:51:54 +0800

    新增面包屑组件

---
+++ src/components/Breadcrumb.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import { ArrowRight } from '@element-plus/icons-vue'
+import { useRoute } from "vue-router";
+
+const route = useRoute()
+const props = defineProps({
+  parentTitle: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <el-breadcrumb :separator-icon="ArrowRight" style="margin-bottom: 15px;">
+    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+    <el-breadcrumb-item>{{ parentTitle }}</el-breadcrumb-item>
+    <el-breadcrumb-item>{{ route.meta['title'] }}</el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
+
+<style scoped>
+
+</style>
